e989dfd1175f18add8687fe283da7b4d014d2f14
Victor Santos <vfreitass16@icloud.com>
2021-08-15 17:45:03 -0300

    implementando a operacao de instanciacao de categorias

--- src/main/java/com/victor/spring/modeloconceitual/ModeloconceitualApplication.java
+++ src/main/java/com/victor/spring/modeloconceitual/ModeloconceitualApplication.java
@@ -1,13 +1,35 @@
 package com.victor.spring.modeloconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victor.spring.modeloconceitual.domain.Categoria;
+import com.victor.spring.modeloconceitual.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModeloconceitualApplication {
+public class ModeloconceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloconceitualApplication.class, args);
+	}
+
+	// Instanciacao dos objetos
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+
 	}
 
 }
