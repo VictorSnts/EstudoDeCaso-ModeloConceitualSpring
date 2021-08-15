1622c5ff7eb1d2dcd26934be6443ff93411e7860
Victor Santos <vfreitass16@icloud.com>
2021-08-15 13:51:57 -0300

    Initial commit

---
+++ src/main/java/com/victor/spring/modeloconceitual/ModeloconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.victor.spring.modeloconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloconceitualApplication.class, args);
+	}
+
+}
