1fc1ba4702c42a812b4c2628464013e9a289096f
Victor Santos <vfreitass16@icloud.com>
2021-08-22 21:08:35 -0300

    Implementando metodo POST de Categoria"

--- src/main/java/com/victor/spring/modeloconceitual/resource/CategoriaResource.java
+++ src/main/java/com/victor/spring/modeloconceitual/resource/CategoriaResource.java
@@ -1,13 +1,16 @@
 package com.victor.spring.modeloconceitual.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.victor.spring.modeloconceitual.domain.Categoria;
 import com.victor.spring.modeloconceitual.services.CategoriaService;
@@ -26,9 +29,18 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(categoria);
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<?> listar() {
 		List<Categoria> categoria = categoriaService.listar();
 		return ResponseEntity.ok().body(categoria);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@RequestBody Categoria categoria) {
+		categoria = categoriaService.inserir(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()			// Pega a URI do ultimo recurso que foi inserido
+				.path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
