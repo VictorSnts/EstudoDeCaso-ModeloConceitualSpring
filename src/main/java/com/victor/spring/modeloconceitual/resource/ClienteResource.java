bf0dc7990da7a035f56321a83528566fa33ce805
Victor Santos <vfreitass16@icloud.com>
2021-08-26 13:46:21 -0300

    Inserindo clientes com metodo POST

--- src/main/java/com/victor/spring/modeloconceitual/resource/ClienteResource.java
+++ src/main/java/com/victor/spring/modeloconceitual/resource/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.victor.spring.modeloconceitual.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.victor.spring.modeloconceitual.domain.Cliente;
 import com.victor.spring.modeloconceitual.dto.ClienteDTO;
+import com.victor.spring.modeloconceitual.dto.NovoClienteDTO;
 import com.victor.spring.modeloconceitual.services.ClienteService;
 
 @RestController
@@ -31,23 +34,13 @@ public class ClienteResource {
 		Cliente cliente = clienteService.buscar(id);
 		return ResponseEntity.ok().body(cliente);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<?> listar() {
 		List<Cliente> clientes = clienteService.listar();
-		List<ClienteDTO> clienteDTO = clientes.stream().map(obj -> new ClienteDTO(obj))
-				.collect(Collectors.toList());
+		List<ClienteDTO> clienteDTO = clientes.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(clienteDTO);
 	}
-
-//	@RequestMapping(method = RequestMethod.POST)
-//	public ResponseEntity<Void> inserir(@Valid @RequestBody ClienteDTO clienteDTO) {
-//		Cliente cliente = clienteService.fromDTO(clienteDTO);
-//		cliente = clienteService.inserir(cliente);
-//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest() // Pega a URI do ultimo recurso que foi inserido
-//				.path("/{id}").buildAndExpand(cliente.getId()).toUri();
-//		return ResponseEntity.created(uri).build();
-//	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> atualizar(@PathVariable Integer id, @Valid @RequestBody ClienteDTO clienteDTO) {
@@ -62,16 +55,24 @@ public class ClienteResource {
 		clienteService.deletar(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<?> listarPage(
-			@RequestParam(value="page", defaultValue = "0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy,  
-			@RequestParam(value="direction", defaultValue = "ASC") String direction
-			) {
+	public ResponseEntity<?> listarPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Cliente> clientes = clienteService.listarPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> clienteDTO = clientes.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(clienteDTO);
 	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@Valid @RequestBody NovoClienteDTO novoClienteDTO) {
+		Cliente cliente = clienteService.fromDTO(novoClienteDTO);
+		cliente = clienteService.inserir(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest() // Pega a URI do ultimo recurso que foi inserido
+				.path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/victor/spring/modeloconceitual/services/ClienteService.java
+++ src/main/java/com/victor/spring/modeloconceitual/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.victor.spring.modeloconceitual.domain.Cidade;
 import com.victor.spring.modeloconceitual.domain.Cliente;
+import com.victor.spring.modeloconceitual.domain.Endereco;
+import com.victor.spring.modeloconceitual.domain.enums.TipoCliente;
 import com.victor.spring.modeloconceitual.dto.ClienteDTO;
+import com.victor.spring.modeloconceitual.dto.NovoClienteDTO;
 import com.victor.spring.modeloconceitual.repositories.ClienteRepository;
+import com.victor.spring.modeloconceitual.repositories.EnderecoRepository;
 import com.victor.spring.modeloconceitual.services.exceptions.DataIntegrityException;
 import com.victor.spring.modeloconceitual.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +28,9 @@ public class ClienteService {
 	// indica que essa dependencia vai ser instanciada automaticamente pelo spring
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente buscar(Integer id) {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
@@ -35,12 +44,7 @@ public class ClienteService {
 			throw new ObjectNotFoundException("Nenhum objeto encontrado!");
 		return clientes;
 	}
-	
-	public Cliente inserir(Cliente cliente) {
-		cliente.setId(null); // Garantindo que o objeto seja uma nova cliente
-		return clienteRepository.save(cliente);
-	}
-	
+
 	public Cliente atualizar(Cliente cliente) {
 		Cliente novoCliente = buscar(cliente.getId());
 		atualizaDados(cliente, novoCliente);
@@ -52,8 +56,7 @@ public class ClienteService {
 		try {
 			clienteRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException(
-					"Não é possivel excluir uma cliente que tenha entidades relacionadas");
+			throw new DataIntegrityException("Não é possivel excluir uma cliente que tenha entidades relacionadas");
 		}
 	}
 
@@ -65,9 +68,36 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO clienteDTO) {
 		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
 	}
-	
-	private void atualizaDados (Cliente cliente, Cliente novoCliente) {
+
+	public Cliente fromDTO(NovoClienteDTO novoClienteDTO) {
+		Cliente cliente = new Cliente(null, novoClienteDTO.getNome(), novoClienteDTO.getEmail(),
+				novoClienteDTO.getCpfOuCnpj(), TipoCliente.toEnum(novoClienteDTO.getTipoCliente()));
+		Cidade cidade = new Cidade(novoClienteDTO.getCidadeId(), null, null);
+		Endereco endereco = new Endereco(null, novoClienteDTO.getLogradouro(), novoClienteDTO.getNumero(),
+				novoClienteDTO.getComplemento(), novoClienteDTO.getBairro(), novoClienteDTO.getCep(), cliente, cidade);
+
+		cliente.getEnderecos().add(endereco);
+		cliente.getTelefones().add(novoClienteDTO.getTel1());
+
+		if (novoClienteDTO.getTel2() != null) {
+			cliente.getTelefones().add(novoClienteDTO.getTel2());
+		}
+		if (novoClienteDTO.getTel3() != null) {
+			cliente.getTelefones().add(novoClienteDTO.getTel3());
+		}
+		return cliente;
+	}
+
+	private void atualizaDados(Cliente cliente, Cliente novoCliente) {
 		novoCliente.setNome(cliente.getNome());
 		novoCliente.setEmail(cliente.getEmail());
+	}
+
+	@Transactional // indica que a inclusao dos dados tem que ocorrer de forma transacional
+	public Cliente inserir(Cliente cliente) {
+		cliente.setId(null);
+		cliente = clienteRepository.save(cliente);
+		enderecoRepository.saveAll(cliente.getEnderecos());
+		return cliente;
 	}
 }
