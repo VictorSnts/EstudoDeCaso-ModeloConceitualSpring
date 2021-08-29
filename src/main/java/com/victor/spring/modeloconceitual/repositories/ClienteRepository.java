cf5aab6e9a932372a79a4f5ef3a2f11354324481
Victor Santos <vfreitass16@icloud.com>
2021-08-28 23:30:06 -0300

    Incluindo validacao de email ja existente no cadastro de clientes

--- src/main/java/com/victor/spring/modeloconceitual/repositories/ClienteRepository.java
+++ src/main/java/com/victor/spring/modeloconceitual/repositories/ClienteRepository.java
@@ -2,11 +2,16 @@ package com.victor.spring.modeloconceitual.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.victor.spring.modeloconceitual.domain.Cliente;
 
 // indica ao JPA que essa interface vai ser capaz de realizar operacoes de acesso dados
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email); // O Spring Data autmaticamente cria o metodo quando usamos o findBy no inicio
+										// do nome do metodo
 
 }
--- src/main/java/com/victor/spring/modeloconceitual/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/victor/spring/modeloconceitual/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.victor.spring.modeloconceitual.domain.Cliente;
 import com.victor.spring.modeloconceitual.domain.enums.TipoCliente;
 import com.victor.spring.modeloconceitual.dto.NovoClienteDTO;
+import com.victor.spring.modeloconceitual.repositories.ClienteRepository;
 import com.victor.spring.modeloconceitual.resource.exception.FieldMessage;
 import com.victor.spring.modeloconceitual.services.validation.utils.br;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, NovoClienteDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (novoClienteDTO.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getId()) && !br.isValidCNPJ(novoClienteDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "O numero de CNPJ nao é valido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(novoClienteDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "O email informado ja esta cadastrado"));
 		}
 
 		// Transforma os erros personalizados(FieldMessage) para os erros do framework
